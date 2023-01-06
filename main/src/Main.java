6c094d0536d590200d9a07a08e18571a86bcaa83
Joshua Miller <118137295+JoshuaMillerIndustries@users.noreply.github.com>
2023-01-06 16:42:19 -0500

    Added new JFrame/viability/size/title/Exit on close function.

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        JFrame frame = new JFrame(); // creating new frame
+
+        frame.setVisible(true); // making sure frame is visible
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // program will end when window closes
+        frame.setSize(720,720); // setting height and with for window
+        frame.setTitle("Cyberdyne Systems Employee Portal ");
 
 
 

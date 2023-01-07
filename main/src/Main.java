5fe9b342830e9ad30c12f8ee9332289631804a45
Joshua Miller <jamiller38@gmail.com>
2023-01-06 20:54:16 -0500

    Added menu bar and panel

--- main/src/Main.java
+++ main/src/Main.java
@@ -3,12 +3,23 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
+
         JFrame frame = new JFrame(); // creating new frame
+        JPanel panel = new JPanel(); // creating panel
+        frame.add(panel); //adding panel to JFrame
+        JMenuBar menuBar = new JMenuBar(); // creating menu bar
+        frame.setJMenuBar(menuBar); // adding menu bar to JFrame
+
+
+
+
+        // GUI preferences
 
         frame.setVisible(true); // making sure frame is visible
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // program will end when window closes
         frame.setSize(820,720); // setting height and with for window
-        frame.setTitle("Cyberdyne Systems Employee Portal ");
+        frame.setTitle("Cyberdyne Systems Employee Portal "); // Title of the frame
+
 
 
 
